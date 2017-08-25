d66d53e9904d0890b341f1fb6e6df77c4e0812e3
Shivangie <30972222+Shivangie@users.noreply.github.com>
2017-08-25 18:35:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,22 @@
 //Counter code
-function modify_qty(val) {
-    var qty = document.getElementById('qty').value;
-    var new_qty = parseInt(qty,10) + val;
+
+button.onclick=function(){
     
-    if (new_qty < 0) {
-        new_qty = 0;
+    //Create a request object 
+    var request=new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status==200)
+            var counter=request.responseText;
+                counter=counter+1;
+               var span=document.getElementById('count');
+               span.innerHTML=counter.toString();
+        }
     }
-    
-    document.getElementById('qty').value = new_qty;
-    return new_qty;
-}
-   
+        //Not done yet
+    };
     // Make the request
     request.open('GET', 'http://http://shivanginldhs.imad.hasura-app.io/', true);
     request.send(null);
